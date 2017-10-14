7dff0707cc3e2e90c26e28e162a16715ddeadb60
AdamKogut <akogut@purdue.edu>
2017-10-14 01:12:11 -0400

    Add base stuff

---
+++ family-cookbook/src/baseExample.js
@@ -0,0 +1,20 @@
+import Rebase from 're-base'
+import firebase from 'firebase/app'
+import database from 'firebase/database'
+import 'firebase/auth'
+
+const app=firebase.initializeApp({
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+})
+
+const db=database(app)
+
+export const auth=app.auth()
+export const githubProvider=new firebase.auth.GithubAuthProvider()
+
+export default Rebase.createClass(db)
