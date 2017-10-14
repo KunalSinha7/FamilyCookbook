01f2753572fb0ef25518778c1ff49eea3141ce12
AdamKogut <akogut@purdue.edu>
2017-10-14 02:02:21 -0400

    Add links, new pages, format things

--- family-cookbook/src/App.js
+++ family-cookbook/src/App.js
@@ -7,17 +7,31 @@ import MyRecipe from './MyRecipe.js';
 import AllRecipe from './AllRecipe.js';
 
 class App extends Component {
+  state={
+    recipes:{},
+    uid:null,
+  }
+
+  isLogin=()=>{
+    this.state.uid==null?`Login`:`Logout`;
+  }
+
   render() {
     return (
       <div>
         <Header/>
         <div className="App">
           <ul className='nav-links'>
-            <li><NavLink to='/MyRecipes'></NavLink></li>
-            <li><NavLink to='/'></NavLink></li>
+            <li><NavLink to='/MyRecipes'>My Recipes</NavLink></li>
+            <li><NavLink to='/AllRecipes'>Community Recipes</NavLink></li>
+            <li><NavLink to='/'>{this.state.uid==null?`Login`:`Logout`}</NavLink></li>
           </ul>
-          
         </div>
+        <Switch>
+          <Route path='/MyRecipes' component={MyRecipe}/>
+          <Route path='/AllRecipes' component={AllRecipe}/>
+          <Route exact path='/'/>
+        </Switch>
       </div>
     );
   }
---
+++ family-cookbook/src/MyRecipe.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MyRecipe extends Component {
+   render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default MyRecipe;
