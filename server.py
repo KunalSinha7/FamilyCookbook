b6c31b9e5670af4e37614d831df75e6fb4f5153b
Kunal Sinha <sinha33@purdue.edu>
2017-10-14 21:56:21 -0400

    Update - everything should work

--- server.py
+++ server.py
@@ -2,8 +2,6 @@ from flask import Flask, jsonify
 import json
 import pyrebase
 
-
-
 #config for pyrebase (python wrapper for firebase)
 config = {
     "apiKey": "AIzaSyD2UPwPcvgPKZ56qXOSAxnmSlUZSPMMsmc",
@@ -13,11 +11,8 @@ config = {
 }
 firebase = pyrebase.initialize_app(config)
 db = firebase.database()
-# db.child("users")
-# db.child("recipes")
+
 print("configured")
-# auth = firebase.auth()
-# user = auth.sign_in_with_email_and_password("kunalsinha98@gmail.com", "soccerRMA7!")
 
 #create flask app
 app = Flask(__name__)
@@ -26,46 +21,35 @@ app = Flask(__name__)
 def hello():
     return "Starting up the cookbook"
 
-@app.route("/addUser/id=<id>&name=<name>",  methods=['POST'])
-def addUser(id, name):
-    data = {'id' : id, 'name' : name}
-    # db.child("users").child(id)
-    result =db.child("users").child(id).set(data)
-    # print (result)
-    return "success"
-
 @app.route("/removeUser/id=<id>", methods=['PUSH'])
 def removeUser(id):
-    db.child("users").child(id).remove()
+    db.child("Users").child(id).remove()
     return "success"
 
-@app.route("/getUser/id=<id>", methods=['GET'])
-def getUser(id):
-    user = db.child("users").child(id).get()
-    return user.val()
+@app.route("/getUserRecipies/userId=<userId>", methods=['GET'])
+def getUserRecipies(userId):
+    myDict = {}
+    user_recipes = db.child("Users").child(userId).get()
+    for r in user_recipes.each():
+        dic = r.val();
+        name = r.key();
+        myDict[name] = dic
+    return json.dumps(myDict)
 
-@app.route("/addRecipe/recipeId=<recipeId>&name=<name>&ownerId=<ownerId>&recipe=<recipe>&favs=<favs>",  methods=['POST'])
-def addRecipe(recipeId, name, ownerId, recipe):
-    data = {'recipeId' : recipeId, 'name' : name, 'ownerId' : ownerId, 'recipe' : recipe, 'favs' : favs}
-    result = db.child("recipes").child(recipeId).set(data)
-    user = db.child("users").child(ownerId).child("recipes").push({"recipeId" : recipeId})
+@app.route("/removeRecipe/recipeId=<recipeId>&userId=<userId>", methods=['PUSH'])
+def removeRecipe(recipeId,userId):
+    db.child("Users").child(userId).child(recipeId).remove()
     return "success"
 
-@app.route("/removeRecipe/recipeId=<recipeId>", methods=['PUSH'])
-def removeRecipe(recipeId):
-    owner = db.child("recipes").child(recipeId).child("ownerId").val()
-    db.child("recipes").child(recipeId).remove()
-    user_recipes = db.child("users").child(owner).child("recipes").get()
-    for rec in user_recipes.each():
-        if(db.child("users").child(owner).child("recipes").val() == recipeId):
-            db.child("users").child(owner).child("recipes").child(db.child("users").child(owner).child("recipes").key()).remove()
-            break
-    return "success"
-
-@app.route("/getRecipe/recipeId=<recipeId>", methods=['PUSH'])
+@app.route("/getRecipe/recipeId=<recipeId>", methods=['GET'])
 def getRecipe(recipeId):
-    return db.child("recipes").child(recipeId).val()
+    myDict = {}
+    rec = db.child("recipes").get()
+    for r in rec.each():
+        dic = r.val()
+        name = r.key()
+        myDict[name] = dic
+    return json.dumps(myDict)
 
-@app.route("/getFavs/recipeId=<recipeId>", methods=['PUSH'])
-def getFavs(recipeId):
-    return db.child("recipe").child(recipeId).child("favs").val()
+if __name__=='__main__':
+    app.run(debug=True)
